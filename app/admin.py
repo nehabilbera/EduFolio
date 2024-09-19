cc4fe52de5c132f92afa4f92f7714fba65d9b908
Neha Gupta <nehabilbera@gmail.com>
2024-09-19 20:05:34 +0530

    create models and database

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Resource, Course, Semester, Subject
 
-# Register your models here.
+@admin.register(Resource)
+class ResourceModelAdmin(admin.ModelAdmin):
+    list_display = ['course_','semester_','subject','resource_type']
+    def course_(self, obj):
+        return obj.subject.semester.course
+    def semester_(self, obj):
+        return obj.subject.semester.semester_number
+    
+admin.site.register(Course)
+
+@admin.register(Semester)
+class SemesterModelAdmin(admin.ModelAdmin):
+    list_display = ['course','semester_number']
+
+
+@admin.register(Subject)
+class SubjectModelAdmin(admin.ModelAdmin):
+    list_display = ['subject_code', 'subject_name', 'semester_', "course_"]
+    
+    def semester_(self, obj):
+        sem = obj.semester.semester_number
+        return sem
+        
+    def course_(self, obj):
+        cr = obj.semester.course.course_name
+        return cr
--- app/models.py
+++ app/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+
+# choices of course_name
+COURSE_CHOICES = (
+    ("MCA", "MCA"),
+    ("BTECH-CSE", "BTECH-CSE"),
+    ("BTECH-ECE", "BTECH-ECE"),
+    ("BTECH-EE", "BTECH-EE"),
+    ("BTECH-ME", "BTECH-ME"),
+)
+
+
+# create course table
+class Course(models.Model):
+    course_name = models.CharField(max_length=100, choices=COURSE_CHOICES)
+
+    def __str__(self):
+        return self.course_name
+
+
+# choices of semester
+SEMESTER_CHOICES = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+)
+
+
+# create course semester
+class Semester(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    semester_number = models.IntegerField(choices=SEMESTER_CHOICES)
+
+    def __str__(self):
+        return str(self.course) + "-" + str(self.semester_number)
+
+
+# create subject table
+class Subject(models.Model):
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    subject_code = models.CharField(max_length=10)
+    subject_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.subject_code} ({self.subject_name})"
+
+
+# choices of types
+RESOURCES_TYPE_CHOICES = (
+    ("PYQ", "PYQ"),
+    ("Reading Material", "Reading Material"),
+    ("Online Resources", "Online Resources")       
+)
+
+
+# create resources table
+class Resource(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    resource_link = models.CharField(max_length=1000)
+    resource_type = models.CharField(max_length=1000, choices=RESOURCES_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.resource_type
